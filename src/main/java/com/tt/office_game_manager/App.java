e7d7a24df5686ac687413c4f4f99379eb3321700
Piotr Frankiewicz <p.frankiewicz@tt.com.pl>
2017-07-14 13:43:50 +0200

    Fixed Autowiring crashing by adding configs to App

--- src/main/java/com/tt/office_game_manager/App.java
+++ src/main/java/com/tt/office_game_manager/App.java
@@ -1,13 +1,22 @@
 package com.tt.office_game_manager;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Hello world!
  */
 
 @SpringBootApplication
+@EntityScan( basePackages = {"com.tt.office_game_manager"})
+@EnableAutoConfiguration
+@EnableConfigurationProperties
+@ComponentScan( basePackages = {"com.tt.office_game_manager"})
 public class App {
     public static void main(String[] args) {
         //TODO: Repository i serwisy do kazdej encji
